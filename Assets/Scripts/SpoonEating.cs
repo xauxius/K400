f6fd15cf5be273b4a14c62bb5773c7f71cfb1de0
EmiKrav <126260437+EmiKrav@users.noreply.github.com>
2024-03-21 20:46:59 +0200

    Sriuba

--- Assets/Scripts/SpoonEating.cs
+++ Assets/Scripts/SpoonEating.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using static UnityEngine.Rendering.DebugUI;
 
 public class SpoonEating : MonoBehaviour
 {
+	private bool antsauksto = false;
+	private void Start()
+	{
+		transform.GetChild(0).gameObject.SetActive(false);
+	}
 
 	[SerializeField] private AudioClip valgymogarsas;
 	void OnCollisionEnter(Collision collision)
@@ -10,14 +17,29 @@ public class SpoonEating : MonoBehaviour
         {
             Eatable eatable = collision.gameObject.GetComponent<Eatable>();
 
-            if (eatable is not null)
+            if (eatable is not null && antsauksto == false)
             {
+				
+				Material newMat = eatable.GetComponent<MeshRenderer>().material;
+				transform.GetChild(0).gameObject.SetActive(true);
+				transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().material = newMat;
 
-				SoundManager.instance.playEfektus(valgymogarsas, transform);
+				antsauksto = true;
+
 				eatable.Eat();
 
 			
 			}
         }
     }
+	public void Valgyti()
+	{
+		if (antsauksto)
+		{
+			antsauksto = false ;
+			transform.GetChild(0).gameObject.SetActive(false);
+			SoundManager.instance.playEfektus(valgymogarsas, transform);
+			
+		}
+	}
 }
