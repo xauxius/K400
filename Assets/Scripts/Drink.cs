34c16715be9c4d5fbfaa03c6a67cf55255a3760e
EmiKrav <126260437+EmiKrav@users.noreply.github.com>
2024-04-15 18:31:06 +0300

    refill

--- Assets/Scripts/Drink.cs
+++ Assets/Scripts/Drink.cs
@@ -11,9 +11,12 @@ public class Drink : MonoBehaviour
 	[SerializeField] private AudioClip drinksound;
 	private bool drinkable = true;
 	private bool drinking = false;
+	bool refiling = false;
 	public Mouth mouth;
 	public PlayableDirector director;
 	public AlembicStreamPlayer streamPlayerScript;
+	public GameObject coffemachine;
+	float timetostop;
 
 	// Starting transform
 	private Vector3 startPosition;
@@ -22,7 +25,8 @@ public class Drink : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
     {
-		director.time = streamPlayerScript.EndTime;
+		director.time = streamPlayerScript.CurrentTime;
+		timetostop = streamPlayerScript.CurrentTime;
 		particles = GameObject.FindGameObjectsWithTag("LiquidParticle");
 	}
 
@@ -45,12 +49,25 @@ public class Drink : MonoBehaviour
 			drinking = false;
 			director.Pause();
 		}
+		if (refiling)
+		{
+			director.Play();
+			streamPlayerScript.CurrentTime = (float)director.time;
+			drinkable = true;
+			if (director.time >= timetostop - 0.025)
+			{
+				streamPlayerScript.CurrentTime = streamPlayerScript.EndTime;
+				director.Pause();
+				StopRefil();
+			}
+		}
 
 	}
 	public void Slurp()
 	{
 		if (drinkable)
 		{
+			director.timeUpdateMode = DirectorUpdateMode.Manual;
 			director.Play();
 			director.time -= (Time.deltaTime + 0.5);
 			streamPlayerScript.CurrentTime = (float)director.time;
@@ -60,9 +77,35 @@ public class Drink : MonoBehaviour
 					par.SetActive(false);
 				}
 				drinkable = false;
-			
+				streamPlayerScript.CurrentTime = 0;
+
 			}
 			SoundManager.instance.playEfektus(drinksound, transform);
 		}
 	}
+	public void Refill()
+	{
+		director.timeUpdateMode = DirectorUpdateMode.GameTime;
+		var distance = Vector3.Distance(coffemachine.transform.position, transform.position);
+		if (distance <= 0.5)
+		{
+			transform.position = coffemachine.transform.position;
+			transform.rotation = coffemachine.transform.rotation;
+			foreach (GameObject par in particles)
+			{
+				par.SetActive(true);
+			}
+			refiling = true;
+			director.initialTime = streamPlayerScript.CurrentTime;
+			director.time = streamPlayerScript.CurrentTime;
+		}
+		else {
+			refiling = false;
+		}
+	}
+	public void StopRefil()
+	{
+		refiling = false;
+	}
+
 }
