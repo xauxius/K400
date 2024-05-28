305bc031c9cdc686db6ced2fdc89020f90f9230d
EmiKrav <126260437+EmiKrav@users.noreply.github.com>
2024-05-29 00:11:50 +0300

    ryziai

--- Assets/Scripts/Eating/EatableChildDestroy.cs
+++ Assets/Scripts/Eating/EatableChildDestroy.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class EatableChildDestroy : EatableBase
 {
-	[SerializeField] private int DisplayCount;
-    public override int GetDisplayCount()
+	private int DisplayCount;
+	[SerializeField] private int removeAmount;
+
+	private void Start()
+	{
+		DisplayCount = transform.childCount;
+		GetDisplayCount();
+	}
+	public override int GetDisplayCount()
     {
         return DisplayCount;
     }
 
     public override void DisplayByIndex(int index)
     {
-        int removeAmount = (int)Math.Ceiling((double)transform.childCount / DisplayCount);
-
+		//  int removeAmount = (int)Math.Ceiling((double)transform.childCount / DisplayCount);
+		DisplayCount = transform.childCount;
+	
+		if (removeAmount > transform.childCount)
+		{
+			removeAmount = transform.childCount;
+		}
 		for (int i = 0; i < removeAmount; i++)
 		{
+			
 			Destroy(transform.GetChild(i).gameObject);
+			
 		}
-    }
+		GetDisplayCount();
+	}
 }
