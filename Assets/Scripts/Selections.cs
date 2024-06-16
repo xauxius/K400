1222660f4e1aa4d9c464a88b0ec36b00530a30c1
EmiKrav <126260437+EmiKrav@users.noreply.github.com>
2024-06-16 16:50:34 +0300

    video

--- Assets/Scripts/Selections.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class Selections
-{
-    public static List<GameObject> Selected = new List<GameObject>();
-}
--- Assets/Scripts/Spoonable.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class Spoonable : MonoBehaviour
-{
-    public Material SpoonedMaterial;
-}
